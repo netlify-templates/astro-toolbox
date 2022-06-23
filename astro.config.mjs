052ea1c797945fccb755f99b95d88bdf42e07ef3
taty2010 <shydalys123@gmail.com>
2022-06-23 14:08:10 -0500

    testing to see if adding adapter works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   adapter: netlify()
+});
