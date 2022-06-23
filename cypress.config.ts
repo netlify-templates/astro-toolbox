022007b8916554c88cdf7c9599015dd68a1ceb8e
taty2010 <shydalys123@gmail.com>
2022-06-23 15:29:08 -0500

    updated baseUrl for cypress tests and fetch request

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000/",
+    baseUrl: process.env.MODE === "production" ? process.env.DEPLOY_URL : "http://localhost:8888",
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
