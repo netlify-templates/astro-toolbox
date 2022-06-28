43395436d77cc23599124b6442b7bc4730d2d43a
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-28 14:04:38 -0400

    chore: Update function for esbuild syntax

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -1,12 +1,6 @@
-const jokes = [
-    "My New Years resolution is to stop leaving things so late.",
-    "Child: Dad, make me a sandwich. Dad: Poof! You're a sandwich.",
-    "The invention of the wheel was what got things rolling",
-    "What kind of music do mummy's like? Rap",
-    "What do you get when you cross a chicken with a skunk? A fowl smell!"
-  ]
+import jokes from './jokes.json'
 
-exports.handler = async () => {
+export const handler = async () => {
     const randomIndex = Math.floor(Math.random() * jokes.length)
     const randomJoke = jokes[randomIndex]
     return {
