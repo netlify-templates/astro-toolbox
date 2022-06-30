495db92c6ae51426dcde75e76ea4f0ce502cf063
Tatyana <43764894+taty2010@users.noreply.github.com>
2022-06-30 16:04:51 -0500

    adds feedback form component and styling (#19)
    * adds feedback form component and styling
    
    * created success page
    
    * added form action to success page and added paragraph going over example
    
    * updated styling to bg, main element position, and footer
    
    * updated test to check for link and not blockquote
    
    * updated root bg in layout and removed unset in index
    
    * updated Readme to include forms and removed import of layout component in form component
    
    Co-authored-by: Tara Z. Manicsic <tzmanics@gmail.com>

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,7 +3,7 @@ describe('Basic specs', () => {
     cy.visit('/')
   })
 
-  it('Check for jokes', () => {
-    cy.get('blockquote').should('be.not.empty')
+  it('Check for jokes link', () => {
+    cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
   })
 })
